2fe1a66764b3b75ac33f8ac2ab731afcd033d14b
jecndlria <jecndlria@gmail.com>
2024-02-11 15:48:31 -0800

    fixing invalid byte issue

--- src/lambda/getS3Object/getS3Object.py
+++ src/lambda/getS3Object/getS3Object.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import base64
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
@@ -10,12 +11,14 @@ def lambda_handler(event, context):
     try:
         response = s3.get_object(Bucket=bucket_name, Key=object_key)
         image_content = response['Body'].read()
+        encoded_image = base64.b64encode(image_content).decode('utf-8')
 
         # Processing can be done here
 
         return {
             'statusCode' : 200,
-            'body': image_content,
+            'body': encoded_image,
+            'isBase64Encoded': True,
             'headers': {
                 'Content-Type': 'image/png'
             }
