4fba72631b655ced2b28a226b1d02ccf4082f498
jecndlria <jecndlria@gmail.com>
2024-02-12 14:19:07 -0800

    trying cog

--- src/lambda/cognitotest/cognitotest.py
+++ src/lambda/cognitotest/cognitotest.py
@@ -1,9 +1,38 @@
 import json
+import boto3
+
+user_pool_id = "hard coded value, don't do this"
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    """
+    Syntax for getting a username from an event (I think, no way to test yet)
+    user_pool_id = event['user_pool_id']
+    client_id = event['callerContext']['clientId']
+    username = event['username']
+    """
+    cognito_client = boto3.client('cognito-idp')
+    try:
+        response = cognito_client.admin_get_user(UserPoolId=user_pool_id, Username="testuser")
+        user_attributes = response['UserAttributes']
+        for attribute in user_attributes:
+            if attribute['Name'] == 'username'
+                return {
+                    'statusCode': 200
+                    'body': json.dumps({
+                            'username': attribute['Value']
+                        })
+                }
+        return {
+            'statusCode': 404,
+            'body': json.dumps({
+                    'message': 'Username attribute not found for the user'
+                })
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'message': 'Error retrieving username: {}'.format(str(e))
+                })
+        }
 
