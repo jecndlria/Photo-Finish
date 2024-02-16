a8a06c3b27059977e9a83b811968a2932b2218e7
jecndlria <jecndlria@gmail.com>
2024-02-16 13:21:25 -0800

    added generate_prompt function

---
+++ src/lambda/generate_prompt/generate_prompt.py
@@ -0,0 +1,10 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
