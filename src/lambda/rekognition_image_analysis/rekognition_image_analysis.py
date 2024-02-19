05c985ea6e27354e5b653b129bc9d9ab28fdbcdd
Angelika Bermudez <aberm028@ucr.edu>
2024-02-18 21:51:54 -0800

    added rekognition/s3 event handler

--- src/lambda/rekognition_image_analysis/rekognition_image_analysis.py
+++ src/lambda/rekognition_image_analysis/rekognition_image_analysis.py
@@ -4,163 +4,30 @@ import boto3
 from botocore.exceptions import ClientError
 import requests
 
-from rekognition_objects import (
-    RekognitionLabel,
-    RekognitionModerationLabel,
-    RekognitionText,
-    show_bounding_boxes,
-    show_polygons,
-)
-
-logger = logging.getLogger(__name__)
-
-
-class RekognitionImage:
-    """
-    Encapsulates an Amazon Rekognition image. This class is a thin wrapper
-    around parts of the Boto3 Amazon Rekognition API.
-    """
-
-    def __init__(self, image, image_name, rekognition_client):
-        """
-        Initializes the image object.
-
-        :param image: Data that defines the image, either the image bytes or
-                      an Amazon S3 bucket and object key.
-        :param image_name: The name of the image.
-        :param rekognition_client: A Boto3 Rekognition client.
-        """
-        self.image = image
-        self.image_name = image_name
-        self.rekognition_client = rekognition_client
-
-
-    @classmethod
-    def from_file(cls, image_file_name, rekognition_client, image_name=None):
-        """
-        Creates a RekognitionImage object from a local file.
-
-        :param image_file_name: The file name of the image. The file is opened and its
-                                bytes are read.
-        :param rekognition_client: A Boto3 Rekognition client.
-        :param image_name: The name of the image. If this is not specified, the
-                           file name is used as the image name.
-        :return: The RekognitionImage object, initialized with image bytes from the
-                 file.
-        """
-        with open(image_file_name, "rb") as img_file:
-            image = {"Bytes": img_file.read()}
-        name = image_file_name if image_name is None else image_name
-        return cls(image, name, rekognition_client)
-
-
-    @classmethod
-    def from_bucket(cls, s3_object, rekognition_client):
-        """
-        Creates a RekognitionImage object from an Amazon S3 object.
-
-        :param s3_object: An Amazon S3 object that identifies the image. The image
-                          is not retrieved until needed for a later call.
-        :param rekognition_client: A Boto3 Rekognition client.
-        :return: The RekognitionImage object, initialized with Amazon S3 object data.
-        """
-        image = {"S3Object": {"Bucket": s3_object.bucket_name, "Name": s3_object.key}}
-        return cls(image, s3_object.key, rekognition_client)
-
-
-    def detect_labels(self, max_labels):
-        """
-        Detects labels in the image. Labels are objects and people.
-
-        :param max_labels: The maximum number of labels to return.
-        :return: The list of labels detected in the image.
-        """
-        try:
-            response = self.rekognition_client.detect_labels(
-                Image=self.image, MaxLabels=max_labels
-            )
-            labels = [RekognitionLabel(label) for label in response["Labels"]]
-            logger.info("Found %s labels in %s.", len(labels), self.image_name)
-        except ClientError:
-            logger.info("Couldn't detect labels in %s.", self.image_name)
-            raise
-        else:
-            return labels
-
-    def detect_moderation_labels(self):
-        """
-        Detects moderation labels in the image. Moderation labels identify content
-        that may be inappropriate for some audiences.
-
-        :return: The list of moderation labels found in the image.
-        """
-        try:
-            response = self.rekognition_client.detect_moderation_labels(
-                Image=self.image
-            )
-            labels = [
-                RekognitionModerationLabel(label)
-                for label in response["ModerationLabels"]
-            ]
-            logger.info(
-                "Found %s moderation labels in %s.", len(labels), self.image_name
-            )
-        except ClientError:
-            logger.exception(
-                "Couldn't detect moderation labels in %s.", self.image_name
-            )
-            raise
-        else:
-            return labels
-
-class RekognitionLabel:
-    """Encapsulates an Amazon Rekognition label."""
-
-    def __init__(self, label, timestamp=None):
-        """
-        Initializes the label object.
-
-        :param label: Label data, in the format returned by Amazon Rekognition
-                      functions.
-        :param timestamp: The time when the label was detected, if the label
-                          was detected in a video.
-        """
-        self.name = label.get("Name")
-        self.confidence = label.get("Confidence")
-        self.instances = label.get("Instances")
-        self.parents = label.get("Parents")
-        self.timestamp = timestamp
-
-class RekognitionModerationLabel:
-    """Encapsulates an Amazon Rekognition moderation label."""
-
-    def __init__(self, label, timestamp=None):
-        """
-        Initializes the moderation label object.
-
-        :param label: Label data, in the format returned by Amazon Rekognition
-                      functions.
-        :param timestamp: The time when the moderation label was detected, if the
-                          label was detected in a video.
-        """
-        self.name = label.get("Name")
-        self.confidence = label.get("Confidence")
-        self.parent_name = label.get("ParentName")
-        self.timestamp = timestamp
-
-    def to_dict(self):
-        """
-        Renders some of the moderation label data to a dict.
-
-        :return: A dict that contains the moderation label data.
-        """
-        rendering = {}
-        if self.name is not None:
-            rendering["name"] = self.name
-        if self.parent_name is not None:
-            rendering["parent_name"] = self.parent_name
-        if self.timestamp is not None:
-            rendering["timestamp"] = self.timestamp
-        return rendering
-
-
+def lambda_handler(event, context): #event parameter should be triggered by s3
+    rekognition = boto3.client('rekognition')
+    s3 = boto3.client('s3')
+    labels_list = []
+    confidence_list = []
+    try:
+        bucket_name = event['Records'][0]['s3']['bucket']['name']
+        object_key = event['Records'][0]['s3']['object']['key']
+        response = rekognition.detect_labels(
+    Image={
+        'S3Object': {
+            'Bucket': bucket_name,
+            'Name': object_key
+        }
+        }
+        )
+        for object in response['Objects']:
+            object_list.append(object['Name'])
+            confidence_list.append(object['Confidence'])
+            print("Objects:", objects_list)
+            print("Confidence Levels:", confidence_list)
+    except ClientError:
+        logger.info("Couldn't detect labels in %s.", self.object_key)
+    except ValueError as ve: # if s3 bucket or object key info is not in the event
+        print(f"ValueError: {ve}")
+    except Exception as e: #handle other exceptions as needed
+        print(f"An unexpected error occurred: {e}")
