99d7b4af7dc63dc13e0df13bd8b1c2eb840b8cf5
Angelika Bermudez <aberm028@ucr.edu>
2024-03-07 01:05:26 -0800

    Added to rekognit and generate prompt

--- src/lambda/generate_prompt/generate_prompt.py
+++ src/lambda/generate_prompt/generate_prompt.py
@@ -1,33 +1,21 @@
 import json
 import boto3
-import mysql.connector
 import random
 import csv
 
 def lambda_handler(event, context):
-    users = get_users_from_mysql()
-
-    for user in users:
-        send_prompt(user['user_id'])
-
-#def get_users_from_mysql():
-#  mysql query to get all users
+    prompt = choose_object('Photo-Finish\src\lambda\AmazonRekognitionAllLabels_v3.0.csv')
+    print(f"Object of the day: {prompt}")
 
 def choose_object(csv_file_path):
     try:
         with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
             objects = list(csv.reader(csvfile))
-            rand_obj = random.choice(objects)[0] #only 1 column
+            rand_obj = random.choice(objects)[0] # only 1 column
 
             return rand_obj
     except Exception as e:
         print(f"Error reading CSV file: {e}")
 
-def send_prompt_to_user(user_id):
-    obj_of_the_day = choose_object('Photo-Finish\src\lambda\generate_prompt\AmazonRekognitionAllLabels_v3.0.csv') #chooses an object from this csv
-
-    prompt = f"Photo object of the day: {obj_of_the_day}"
-    print(f"Sending prompt to user {user_id}: {prompt}")
-    #how to push notifs on app?? 
 
 
--- src/lambda/rekognition_image_analysis/rekognition_image_analysis.py
+++ src/lambda/rekognition_image_analysis/rekognition_image_analysis.py
@@ -4,6 +4,8 @@ import boto3
 from botocore.exceptions import ClientError
 import requests
 
+from generate_prompt import choose_object #cam i do thhis..
+
 def lambda_handler(event, context): #event parameter should be triggered by s3
     rekognition = boto3.client('rekognition')
     s3 = boto3.client('s3')
@@ -22,17 +24,36 @@ def lambda_handler(event, context): #event parameter should be triggered by s3
         }
         }
         )
-        for object in response['Objects']:
-            object_list.append(object['Name'])
-            confidence_list.append(object['Confidence'])
-            print("Objects:", objects_list)
-            print("Confidence Levels:", confidence_list)
-        sns_topic_arn = 'arn:aws:sns:us-west-1:058264131615:AmazonRekognitionTest'
-        sns.publish(
-            TopicArn=sns_topic_arn,
-            Message=f"Detected labels: {labels_list}, Confidence levels: {confidence_list}", #prompt not setup yet, but should send event notif to sns when the specific object in the prompt is detected in the image
-            Subject="Rekognition Labels Detected"
-        )
+
+        #for label in response['Labels']:
+            #labels_list.append(label['Name'])
+            #confidence_list.append(label['Confidence'])
+
+        #print("Labels:", labels_list)
+        #print("Confidence Levels:", confidence_list)
+
+        #TODO: UM does this make sure that its the same object generated from the generate_prompt????? i am so bad at coding
+        prompt_object = choose_object('Photo-Finish\src\lambda\AmazonRekognitionAllLabels_v3.0.csv')
+        confidence_of_obj = None
+
+        for label in response['Labels']:
+            if label['Name'] == prompt_object:
+                confidence_of_obj = label['Confidence']
+                print(f"Object of the day: {prompt_object}")
+                print(f"Confidence of the object of the day: {confidence_of_obj}")
+            else:
+                print(f"Object not found in image")
+        
+        #if the object is found then it will publish an sns message 
+        if prompt_object in response['Labels']:
+            sns_topic_arn = 'arn:aws:sns:us-west-1:058264131615:prompt_check'
+            sns.publish(
+                TopicArn=sns_topic_arn,
+                Message=f"Detected label: {prompt_object}, Confidence level: {confidence_of_obj}",
+                Subject="Prompt Object Found"
+            )
+            break
+
     except ClientError:
         logger.info("Couldn't detect labels in %s.", self.object_key)
     except ValueError as ve: # if s3 bucket or object key info is not in the event
@@ -40,4 +61,3 @@ def lambda_handler(event, context): #event parameter should be triggered by s3
     except Exception as e: #handle other exceptions as needed
         print(f"An unexpected error occurred: {e}")
     
-    #TODO: send an sns alert when the object in the prompt is detected in the s3 image
