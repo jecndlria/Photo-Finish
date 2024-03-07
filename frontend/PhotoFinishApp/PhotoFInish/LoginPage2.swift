b85f0a45b1c8940d9e31a0af2d9fdd0e5015679d
Yuchen <yzhu133@ucr.edu>
2024-03-06 22:52:07 -0800

    created create account page awaiting lambda completion

--- frontend/PhotoFinishApp/PhotoFInish/LoginPage2.swift
+++ frontend/PhotoFinishApp/PhotoFInish/LoginPage2.swift
@@ -12,6 +12,7 @@ import UIKit
 struct LoginPage2: View {
     @State private var selectedTab = 0
     
+    
     var body: some View {
         TabView(selection: $selectedTab) {
             FirstScreenView()
