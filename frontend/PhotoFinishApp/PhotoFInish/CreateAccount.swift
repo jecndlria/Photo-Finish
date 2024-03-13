b538c78fe987dca46fe1b8612f187de1fe61a8cf
Yuchen <yzhu133@ucr.edu>
2024-03-12 21:21:28 -0700

    create account lambda working

--- frontend/PhotoFinishApp/PhotoFInish/CreateAccount.swift
+++ frontend/PhotoFinishApp/PhotoFInish/CreateAccount.swift
@@ -4,35 +4,54 @@ import AWSCore
 import AWSLambda
 
 
+
+
 struct CreateAccount: View {
     
     
     @State private var username = ""
+    @State private var email = ""
     @State private var password = ""
     @State private var password2 = ""
     @State private var showingLoginScreen = false
     @State private var passwordsNoMatch = 0
-    func createUser(username: String, password: String, password2: String){
-        if password == password2{
-            passwordsNoMatch = 0
-            let lambda = AWSLambda.default()
-            let request = AWSLambdaInvocationRequest()
-            request!.functionName = "test"
-            request!.invocationType = .requestResponse
-            request!.payload = "{\"username\": \"\(username)\", \"password\": \"\(password)\"}".data(using: .utf8)
-            lambda.invoke(request!) { (response, error) in
-                        if let error = error {
-                            print("Error invoking Lambda function: \(error)")
-                        } else if let payload = response?.payload {
-                            // Handle the response payload here
-                            print("Lambda function response: \(payload)")
+    @State private var usernameBad = 0;
+    @State private var printedOutput = ""
+    struct RedirectedOutputStream: TextOutputStream {
+            var target: CreateAccount
+
+            mutating func write(_ string: String) {
+                target.printedOutput.append(string)
+            }
+        }
+    func createUser(username: String, email: String, password: String){
+        let lambda = AWSLambda.default()
+        let request = AWSLambdaInvocationRequest()
+        request!.functionName = "create_account"
+        request!.invocationType = .requestResponse
+        request!.payload = "{\"username\": \"\(username)\", \"name\": \"\(username)\", \"email\": \"\(email)\", \"password\": \"\(password)\"}".data(using: .utf8)
+        lambda.invoke(request!) { (response, error) in
+                    if let error = error {
+                        print("Error invoking Lambda function: \(error)")
+                    } else if let payload = response?.payload {
+                        //as? Data, let payloadString = String(data: payload, encoding: .utf8)
+                        // Handle the response payload here
+                        print("Lambda function response: \(payload)")
+                        var outputStream = RedirectedOutputStream(target: self)
+                        print("Lambda function response: \(payload)", to: &outputStream)
+                        print("Captured output: \(printedOutput)")
+                        if printedOutput.contains("200") {
+                            printedOutput = ""
+                            showingLoginScreen = true
+                        }else if printedOutput.contains("500"){
+                            usernameBad = 2
+                            printedOutput = ""
+                        }else{
+                            passwordsNoMatch = 2
+                            printedOutput = ""
                         }
                     }
-            
-            showingLoginScreen = true
-        }else{
-            passwordsNoMatch = 2
-        }
+                }
     }
     var body: some View{
         ZStack{
@@ -53,6 +72,17 @@ struct CreateAccount: View {
                     .cornerRadius(10)
                     .autocorrectionDisabled(true)
                     .autocapitalization(.none)
+                    .border(.red, width:CGFloat(usernameBad))
+                TextField("Email", text: $email)
+                    .padding()
+                    .frame(width:250, height:40)
+                    .background(Color.white.opacity(0.08))
+                    .foregroundStyle(.red)
+                    .font(.subheadline)
+                    .cornerRadius(10)
+                    .autocorrectionDisabled(true)
+                    .autocapitalization(.none)
+                    .border(.red, width:CGFloat(passwordsNoMatch))
                 SecureField("Password", text: $password)
                     .padding()
                     .frame(width:250, height:40)
@@ -74,7 +104,13 @@ struct CreateAccount: View {
                     .autocapitalization(.none)
                     .border(.red, width:CGFloat(passwordsNoMatch))
                 Button("Create Account"){
-                    createUser(username: username, password: password, password2: password2)
+                    if (password == password2) && (username != "") && (email != ""){
+                        passwordsNoMatch = 0;
+                        createUser(username: username, email: email, password: password)
+                        
+                    }else if password != password2{
+                        passwordsNoMatch = 2;
+                    }
                 }
                     .frame(width:200, height:30)
                     .background(Color.blue.opacity(0.2))
