ddc1d6b950a7fddddbf09e0080e5a1018595de13
jecndlria <jecndlria@gmail.com>
2024-03-13 16:56:49 -0700

    rekog

---
+++ src/lambda/rekognition_image_analysis/password.py
@@ -0,0 +1,6 @@
+# AWS Database 
+rds_host = "pf-database.cz8q4q06c8d4.us-west-1.rds.amazonaws.com" #endpoint
+db_username = "pfadmin"
+db_password = "20093Bb*"
+db_name = "pf_database"
+userpool = "us-west-1_eaAiwMm3i"
--- src/lambda/rekognition_image_analysis/rekognition_image_analysis.py
+++ src/lambda/rekognition_image_analysis/rekognition_image_analysis.py
@@ -2,18 +2,22 @@ import logging
 from pprint import pprint
 import boto3
 from botocore.exceptions import ClientError
-import requests
+from botocore.vendored import requests
 
-def lambda_handler(event, context): #event parameter should be triggered by s3
+def lambda_handler(event, context): #event parameter should be triggered by s3 object creation
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
     rekognition = boto3.client('rekognition')
     s3 = boto3.client('s3')
     sns = boto3.client('sns')
+    bucket_name = 'photo-finish-bucket'
 
+    points_for_photo = 0
     labels_list = []
     confidence_list = []
     try:
-        bucket_name = event['Records'][0]['s3']['bucket']['name']
-        object_key = event['Records'][0]['s3']['object']['key']
+        object_key = event["Name"]
+        logger.info("Calling rekognition.detect_labels function")
         response = rekognition.detect_labels(
     Image={
         'S3Object': {
@@ -22,6 +26,7 @@ def lambda_handler(event, context): #event parameter should be triggered by s3
         }
         }
         )
+        logger.info("Rekognition.detect_labels function executed successfully")
 
         #for label in response['Labels']:
             #labels_list.append(label['Name'])
@@ -31,33 +36,34 @@ def lambda_handler(event, context): #event parameter should be triggered by s3
         #print("Confidence Levels:", confidence_list)
 
         #TODO: retrieve the prompt object from the database
-        prompt_object = " "
+        prompt_object = "Apple"
         confidence_of_obj = None
 
         for label in response['Labels']:
             if label['Name'] == prompt_object:
                 confidence_of_obj = label['Confidence']
-                print(f"Object of the day: {prompt_object}")
-                print(f"Confidence of the object of the day: {confidence_of_obj}")
-            else:
-                print(f"Object not found in image")
+                logger.info(f"Object of the day: {prompt_object}")
+                logger.info(f"Confidence of the object of the day: {confidence_of_obj}")
+                break
+            else: 
+                logger.info(f"Object not found in image")
         
         #if the object is found then it will publish an sns message 
-        if prompt_object in response['Labels']:
+        if confidence_of_obj is not None and confidence_of_obj > 0:
             sns_topic_arn = 'arn:aws:sns:us-west-1:058264131615:prompt_check'
             sns.publish(
                 TopicArn=sns_topic_arn,
                 Message=f"Detected label: {prompt_object}, Confidence level: {confidence_of_obj}",
                 Subject="Prompt Object Found"
             )
-            break
+        #if confidence level above a 50% threshold then points awarded -> update database
+        if confidence_of_obj is not None and confidence_of_obj > 0:
+            points_for_photo += 3
+            #TODO:update database with points count
 
-        #if confidence level above 50% then points awarded -> update database
-
-    except ClientError:
-        logger.info("Couldn't detect labels in %s.", self.object_key)
-    except ValueError as ve: # if s3 bucket or object key info is not in the event
-        print(f"ValueError: {ve}")
-    except Exception as e: #handle other exceptions as needed
-        print(f"An unexpected error occurred: {e}")
-    
+    except KeyError as e:
+        logger.error(f"KeyError: {e}")
+    except ClientError as e:
+        logger.error(f"Error detecting labels: {e}")
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
