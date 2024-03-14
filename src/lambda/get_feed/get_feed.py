45a5f5bd685f3ab42df7bf15ff5f60cfbcac80e4
jecndlria <jecndlria@gmail.com>
2024-03-13 17:51:32 -0700

    feed TEHESE NUTSgit add .git add .git add .git add .git add .

--- src/lambda/get_feed/get_feed.py
+++ src/lambda/get_feed/get_feed.py
@@ -16,17 +16,18 @@ def lambda_handler(event, context):
     conn = pymysql.connect(host=rds_host, user=name, passwd=dbpassw, db='photofinish', connect_timeout=15)
     curr = conn.cursor()
     sql = f"""
-      SELECT dp.photourl
+      SELECT f.friend, dp.photourl
       FROM daily_photos dp
       JOIN friends f ON dp.username = f.friend
-      WHERE f.username = {username}
+      WHERE f.username = '{username}'
     """ 
     curr.execute(sql)
+    print("why?")
     result = curr.fetchall()
     if not result:
       raise ValueError(f"No photos found for friends of the user {username}.")
     for x in range(len(result)):
-      data[f"photo{x}"] = result[x][0]
+      data[result[x][0]] = result[x][1]
     print(data)
   except (Exception, pymysql.DatabaseError) as error:
     print(error)
